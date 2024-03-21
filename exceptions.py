8a058250e938a0a3de9b3c28c61b56ec0eb072ff
QuickLike <QuickLikerfree@gmail.com>
2024-03-21 11:40:26 +0300

    Done... Almost...

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,6 @@
 class HTTPStatusNotOK(Exception):
     pass
+
+
+class RequestError(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -6,12 +6,34 @@ import time
 
 from dotenv import load_dotenv
 import requests
-from telegram import Bot
+import telegram
 
-from exceptions import HTTPStatusNotOK
+from exceptions import HTTPStatusNotOK, RequestError
+
+
+TOKENS_ERROR = 'Не валидные переменные окружения: {}!'
+STATUS_VALUE_ERROR = 'Неожиданное значение ключа "status" - {}'
+RESPONSE_NOT_DICT_ERROR = 'Объект HTTP-ответа должен быть словарем, вместо {}!'
+HOMEWORKS_NOT_LIST_ERROR = 'Объект "homeworks" должен быть списком, вместо {}!'
+HOMEWORKS_NOT_IN_DICT_ERROR = 'Ключ "homeworks" отсутствует в словаре!'
+HOMEWORK_NAME_NOT_IN_DICT_ERROR = 'Ключ "homework_name" отсутсвует в словаре!'
+RESPONSE_CODE_ERROR = '''Страница загружена с ошибками! Код страницы {0}.
+                         Параметры запроса:
+                         url: {1};
+                         headers: {2};
+                         params: {3}.'''
+REQUEST_ERROR = '''Ошибка при подключении к странице {0}!
+                   Параметры запроса:
+                   headers: {1};
+                   params: {2}.'''
+
+STATUS_HAS_CHANGED = 'Изменился статус проверки работы "{0}". {1}'
+SEND_MESSAGE_SUCCESS = 'Успешная отправка сообщения: {}.'
 
 
 load_dotenv()
+
+ENV_VARS = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -32,21 +54,19 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     """Проверка токенов."""
-    env_vars = [
-        'PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID'
-    ]
-    invalid_env_vars = [var for var in env_vars if not globals().get(var)]
-    message = f'Не валидные переменные окружения: {invalid_env_vars}'
+    invalid_env_vars = [var for var in ENV_VARS if not globals().get(var)]
     if invalid_env_vars:
-        logging.critical(message)
-        raise ValueError(message)
-    return not invalid_env_vars
+        logging.critical(TOKENS_ERROR.format(invalid_env_vars))
+        raise ValueError(TOKENS_ERROR.format(invalid_env_vars))
 
 
 def send_message(bot, message):
     """Отправка сообщения бота в Telegram."""
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-    logging.debug('Успешная отправка сообщения.')
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except telegram.error.TelegramError as error:
+        logging.error(error, exc_info=True)
+    logging.debug(SEND_MESSAGE_SUCCESS.format(message))
 
 
 def get_api_answer(timestamp):
@@ -59,39 +79,35 @@ def get_api_answer(timestamp):
     try:
         response = requests.get(**params)
     except requests.RequestException:
-        raise Exception(
-            f'Ошибка при подключении к странице {params.get("url")}'
-        )
+        raise RequestError(REQUEST_ERROR.format(
+            params.get("url"),
+            params.get("headers"),
+            params.get("params")
+        ))
 
     response_code = response.status_code
     if response_code != HTTPStatus.OK:
-        logging.error(
-            f'Ошибка при загрузке страницы. {params}'
-        )
-        raise HTTPStatusNotOK(
-            f'Страница загружена с ошибками! Код страницы {response_code}'
-        )
+        raise HTTPStatusNotOK(RESPONSE_CODE_ERROR.format(
+            response_code,
+            params.get("url"),
+            params.get("headers"),
+            params.get("params")))
     json = response.json()
     for key in ['error', 'code']:
         if key in json:
-            logging.error(f'При загрузке страницы возникла ошибка {json[key]}')
-            return
+            raise KeyError(f'JSON содержит {key}')
     return json
 
 
 def check_response(response):
     """Проверка HTTP-ответа."""
     if not isinstance(response, dict):
-        raise TypeError(
-            f'Объект HTTP-ответа должен быть словарем, вместо {type(response)}'
-        )
+        raise TypeError(RESPONSE_NOT_DICT_ERROR.format(type(response)))
     if 'homeworks' not in response:
-        raise KeyError('Ключ "homeworks" отсутсвует в словаре!')
+        raise KeyError(HOMEWORKS_NOT_IN_DICT_ERROR)
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
-        raise TypeError(
-            f'Объект "homeworks" должен быть списком, вместо {type(homeworks)}'
-        )
+        raise TypeError(HOMEWORKS_NOT_LIST_ERROR.format(type(homeworks)))
     return response
 
 
@@ -99,36 +115,33 @@ def parse_status(homework):
     """Получение статуса проверки домашнего задания."""
     status = homework['status']
     if 'homework_name' not in homework:
-        raise KeyError('Ключ "homework_name" отсутсвует в словаре!')
+        raise KeyError(HOMEWORK_NAME_NOT_IN_DICT_ERROR)
     if status not in HOMEWORK_VERDICTS:
-        raise ValueError(f'Неожиданное значение ключа "status" - {status}')
-    verdict = HOMEWORK_VERDICTS[homework['status']]
-    return ('Изменился статус проверки работы '
-            f'"{homework["homework_name"]}". {verdict}')
+        raise ValueError(STATUS_VALUE_ERROR.format(status))
+    return STATUS_HAS_CHANGED.format(
+        homework["homework_name"],
+        HOMEWORK_VERDICTS[homework['status']]
+    )
 
 
 def main():
     """Основная логика работы бота."""
-    try:
-        check_tokens()
-    except Exception as error:
-        logging.critical(f'Возникла ошибка: {error}')
-        raise ValueError('Отсутствуют переменные окружения!')
-    bot = Bot(token=TELEGRAM_TOKEN)
+    check_tokens()
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
     timestamp = int(time.time())
-    current_status = ''
+    current_verdict = ''
     while True:
         try:
             response = get_api_answer(timestamp)
-            timestamp = response['current_date']
             homeworks = check_response(response)['homeworks']
             if homeworks:
-                status = homeworks[0]['status']
-                if current_status != status:
+                verdict = parse_status(homeworks[0])
+                if current_verdict != verdict:
                     verdict = parse_status(homeworks[0])
-                    current_status = status
+                    current_verdict = verdict
                     send_message(bot, verdict)
+                    timestamp = response.get('current_date', timestamp)
                 else:
                     logging.debug('Статус проверки не изменился')
         except Exception as error:
@@ -141,9 +154,6 @@ def main():
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
-    logger.addHandler(
-        logging.StreamHandler(stream=sys.stdout)
-    )
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
